3cd555c0308703f46ec75808e38cb2e0cbca8578
NaifKAbdullah <Naif-Abdullah@outlook.sa>
2024-05-05 19:29:36 +0300

    Implemented animated typing, but we will attempt to style the last word:

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,42 @@
 import { Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 import Header from "./Components/Header.jsx";
+import { TypeAnimation } from "react-type-animation";
 
 function App() {
   return (
     <div id="parent-container">
       <Header />
-      <section id="hero-section" className="hero-container">
-        <Container fluid>
-          <Row className="d-flex align-items-center" style={{ height: '1080px' }}>
-            <Col 
-              xs={12} 
-              md={6} 
-              className="intro-text"
-            >
-              <div>INTRO TODO</div>
+
+      <section id="hero-section">
+        <Container fluid className="hero-container-kek">
+          <Row
+            className="d-flex align-items-center"
+            style={{ height: "1080px" }}
+          >
+            <Col xs={12} md={6} className="intro-text">
+              <div>
+                <TypeAnimation
+                  sequence={[
+                    // Same substring at the start will only be typed out once, initially
+                    "Solving Complex Problems with Technology",
+                    2000, // wait 1s before replacing "Mice" with "Hamsters"
+                    "Solving Complex Problems with Innovation",
+                    2000,
+                    "Solving Complex Problems with Intellect",
+                    2000,
+                    "Solving Complex Problems with Creativity",
+                    2000,
+                  ]}
+                  wrapper="span"
+                  speed={20}
+                  className="tagline"
+                  repeat={Infinity}
+                />
+              </div>
             </Col>
 
-            <Col 
-              xs={12} 
-              md={6} 
-              className="logo-col"
-            >
+            <Col xs={12} md={6} className="logo-col">
               <div className="logo-container">
                 <img
                   src="./assets/logo.png"
@@ -42,4 +57,3 @@ function App() {
 }
 
 export default App;
-// <img src='./assets/logo.png' alt='Brand Logo'/>
