6982a378e242a9769eaf17ef61e217f161703c19
NaifKAbdullah <Naif-Abdullah@outlook.sa>
2024-05-05 16:02:47 +0300

    Added logo, need to shift it left a little and add the introductory tag line and call-to-action button

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
-
-
-import './App.css'
-import Header from './Components/Header.jsx'
+import { Col, Container, Row } from "react-bootstrap";
+import "./App.css";
+import Header from "./Components/Header.jsx";
 
 function App() {
-
   return (
-    <>
     <div id="parent-container">
-      <Header/>
+      <Header />
+      <section id="hero-section" className="hero-container">
+        <Container fluid>
+          <Row className="d-flex align-items-center" style={{ height: '1080px' }}>
+            <Col 
+              xs={12} 
+              md={6} 
+              className="intro-text"
+            >
+              <div>INTRO TODO</div>
+            </Col>
 
-      <section id="hero-section">
-        <div id="hero-container">
-          <div id="intor text and button TODO">
-
-          </div>
-          <div id='logo'>
-            <img src="./assets/logo.png" alt='Brand Logo'/>
-          </div>
-        </div>
+            <Col 
+              xs={12} 
+              md={6} 
+              className="logo-col"
+            >
+              <div className="logo-container">
+                <img
+                  src="./assets/logo.png"
+                  alt="Brand Logo"
+                  className="responsive-logo"
+                />
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </section>
 
       <section id="experience-section">
         <div className="parallax"></div>
       </section>
-
-
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
+// <img src='./assets/logo.png' alt='Brand Logo'/>
