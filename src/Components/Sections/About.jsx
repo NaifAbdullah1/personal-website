ffa6669081f71e2a68e51fcf65f427f10fb1adf5
NaifKAbdullah <Naif-Abdullah@outlook.sa>
2024-05-16 20:42:14 +0300

    Attempting to replace bootstrap with MUI's grid system

--- src/Components/Sections/About.jsx
+++ src/Components/Sections/About.jsx
@@ -11,22 +11,31 @@ const About = () => {
           <Col xs={12} sm={12} md={12} lg={3} style={{ textAlign: "center" }}>
             <h1 className="sticky-1st-column">About 📝</h1>
           </Col>
-          <Col xs={12} sm={12} md={12} lg={9} style={{ textAlign: "left", display: "flex" }}>
-            <div>
-              <h4>Hello 👋🏻</h4>
-              <br></br>
-              <h4>My name is Naif Abdullah.</h4>
-              <br></br>
-              <p>
-                As a Computer Science graduate, I&apos;ve excelled both academically
-                and as a Full Stack Web Developer. I&apos;ve contributed to critical
-                systems at my university&apos;s IT department and enhanced
-                applications at the J. M. Smucker Co. I&apos;m dedicated to self-learning
-                and collaborative problem-solving in digital
-                environments.
-              </p>
-            </div>
-            <img src="./assets/naif.png" className="naif-image"/>
+          <Col
+            xs={12}
+            sm={12}
+            md={12}
+            lg={9}
+            style={{ textAlign: "left", display: "flex" }}
+          >
+            <Row className="stack">
+              <Col>
+                <h4>Hello 👋🏻</h4>
+                <br></br>
+                <h4>My name is Naif Abdullah.</h4>
+                <p>
+                  As a Computer Science graduate, I&apos;ve excelled both
+                  academically and as a Full Stack Web Developer. I&apos;ve
+                  contributed to critical systems at my university&apos;s IT
+                  department and enhanced applications at the J. M. Smucker Co.
+                  I&apos;m dedicated to self-learning and collaborative
+                  problem-solving in digital environments.
+                </p>
+              </Col>
+              <Col className="center-image">
+                <img src="./assets/naif.png" className="naif-image" />
+              </Col>
+            </Row>
           </Col>
         </Row>
         <h1>Below row</h1>
