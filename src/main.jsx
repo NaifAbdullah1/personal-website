46f599425d346140b4e59687c7c903456fb292b8
NaifKAbdullah <Naif-Abdullah@outlook.sa>
2024-05-16 23:28:27 +0300

    Unifying font across app. Continue by working on the font in the 'about' section. use dev tools to verify font has been applied

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.scss' // Our custom global styles
-import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS import
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import theme from "./theme.jsx";
+import { ThemeProvider } from "@mui/material/styles";
+import "./index.scss"; // Our custom global styles
+import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap CSS import
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
