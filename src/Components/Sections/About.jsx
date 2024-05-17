3ccb1cc0642b2bfe0fd3380cf3c4aa5da2fd0777
NaifKAbdullah <Naif-Abdullah@outlook.sa>
2024-05-17 20:13:08 +0300

    'About' Section Implemented. Moving on to 'Experience', start from left to right. Consult the previous code from the "About" section for speedy development

--- src/Components/Sections/About.jsx
+++ src/Components/Sections/About.jsx
@@ -1,44 +1,108 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Grid, Typography, Box } from "@mui/material";
 
 import "../../scss/Sections/common.scss";
 import "../../scss/Sections/about.scss";
 
 const About = () => {
+  const naifImageProperties = {
+    height: "auto",
+    width: { xs: "50%", sm: "50%", md: "100%", lg: "100%" },
+    pt: { xs: "2rem", sm: "2rem", md: "0px", lg: "0px" },
+  };
+
+  const certifiedByGridItemProperties = {
+    display: "flex",
+    pt: { xs: "2rem", sm: "2rem", md: "0px", lg: "0px" },
+  };
+
   return (
     <section id="about" className="about-section">
-      <Container fluid className="container-layout">
-        <Row style={{ width: "100%" }}>
-          <Col xs={12} sm={12} md={12} lg={3} style={{ textAlign: "center" }}>
-            <h1 className="sticky-1st-column">About 📝</h1>
-          </Col>
-          <Col
+      <Container maxWidth={false} className="container-layout">
+        <Grid container style={{ width: "100%" }}>
+          <Grid
+            item
             xs={12}
             sm={12}
-            md={12}
-            lg={9}
-            style={{ textAlign: "left", display: "flex" }}
+            md={3}
+            lg={3}
+            style={{ textAlign: "center" }}
           >
-            <Row className="stack">
-              <Col>
-                <h4>Hello 👋🏻</h4>
-                <br></br>
-                <h4>My name is Naif Abdullah.</h4>
-                <p>
+            <Typography variant="h1" className="sticky-1st-column">
+              About 📝
+            </Typography>
+          </Grid>
+
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={9}
+            lg={9}
+            style={{
+              textAlign: "left",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Grid container>
+              <Grid item xs={12} sm={12} md={6} lg={6}>
+                <Typography variant="h4">Hello 👋🏻</Typography>
+                <br />
+                <Typography variant="h4">My name is Naif Abdullah</Typography>
+                <br />
+                <Typography variant="body1">
                   As a Computer Science graduate, I&apos;ve excelled both
                   academically and as a Full Stack Web Developer. I&apos;ve
                   contributed to critical systems at my university&apos;s IT
                   department and enhanced applications at the J. M. Smucker Co.
                   I&apos;m dedicated to self-learning and collaborative
                   problem-solving in digital environments.
-                </p>
-              </Col>
-              <Col className="center-image">
-                <img src="./assets/naif.png" className="naif-image" />
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-        <h1>Below row</h1>
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={12} md={6} lg={6} className="center-image">
+                <Box
+                  component="img"
+                  src="./assets/naif.png"
+                  alt="Naif Abdullah"
+                  sx={naifImageProperties}
+                ></Box>
+              </Grid>
+            </Grid>
+
+            <Grid item sx={certifiedByGridItemProperties}>
+
+              <Grid container sx={{ display: "flex", flexDirection: "column"}}>
+                <Grid item sx={{mb: "30px", display: "flex",     justifyContent: { xs: "center", sm: "center", md: "start", lg: "start" }, }}>
+                  <Typography variant="h4">Certified by</Typography>
+                </Grid>
+
+                <Grid item sx={{ display: "flex", 
+                  justifyContent: {xs: "center", sm: "center", md: "start", lg: "start"}, 
+                  gap: "10px"}}>
+                  <Box
+                    component="img"
+                    src="./assets/certifications/doroob-logo.jpg"
+                    style={{
+                      width: "20%",
+                      height: "auto",
+                      borderRadius: "20px",
+                    }}
+                  />
+                  <Box
+                    component="img"
+                    src="./assets/certifications/doroob-logo.jpg"
+                    style={{
+                      width: "20%",
+                      height: "auto",
+                      borderRadius: "20px",
+                    }}
+                  />
+                </Grid>
+              </Grid>
+
+            </Grid>
+          </Grid>
+        </Grid>
       </Container>
     </section>
   );
--- src/theme.jsx
+++ src/theme.jsx
@@ -1,27 +1,16 @@
 import { createTheme } from "@mui/material";
 
 const theme = createTheme({
-    typography: {
-        fontFamily: "'League Spartan', sans-serif",
-    },
+  typography: {
+    fontFamily: "'League Spartan', sans-serif",
     h1: {
-      fontFamily: "'League Spartan', sans-serif",
-    },
-    h2: {
-      fontFamily: "'League Spartan', sans-serif",
-    },
-    h3: {
-      fontFamily: "'League Spartan', sans-serif",
+      fontSize: "2rem",
+      fontWeight: "bold",
     },
     h4: {
-      fontFamily: "'League Spartan', sans-serif",
-    },
-    h5: {
-      fontFamily: "'League Spartan', sans-serif",
-    },
-    h6: {
-      fontFamily: "'League Spartan', sans-serif",
+      fontSize: "1.7rem",
     }
+  },
 });
 
 export default theme;
