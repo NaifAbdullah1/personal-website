66bb07bc9d4a713849fb45cae46f1df4e13accdc
NaifKAbdullah <Naif-Abdullah@outlook.sa>
2024-06-04 13:39:22 +0300

    Added a note, now, fill in the details of the DoIT role using cards

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,10 @@
 
 - For the remaining sections, consider implementing this: https://codesandbox.io/p/sandbox/sticky-parallax-3hsg6?file=%2Fsrc%2Findex.tsx
 Here's the full doc: https://www.react-spring.dev/docs/components/parallax
+
+-In the experience section, we want to display company, tenure, and company link info, rle name, etc... 
+Consider addign some hover animation on the company logo cards wherein if the user hovers over the card, the card either will fade into the details above. Just make sure to make it onClick (rather than onHover) when on mobile. Use the isOnMobile react library for this one. 
+Alternatively, we can make clicking the card result in expanding it with further information  
 */
 
 import { useEffect, useState } from "react";
