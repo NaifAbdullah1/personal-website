7b123c3614baea143de971bfc61d3133b3b27a2f
NaifKAbdullah <Naif-Abdullah@outlook.sa>
2024-06-17 22:24:11 +0300

    Updated Notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 /*
 
-- Wasn't able to make a slideshow for mobile. We may resort to either not showing the slideshow on mobile. Or showing it but disabling the hover effect, we'll also need to make it smaller. 
-EDIT Jun 17th: This library is amazing: https://www.npmjs.com/package/react-photo-view
-We might even replace the marquee with this one if it will offer significant performance boost. 
-
 - Consider offloading the styling of the sections onto a different file/s
 
 - Implement this on the hero page: https://react-scroll-parallax.damnthat.tv/docs/examples/advanced-banners
@@ -15,28 +11,11 @@ Here's the full doc: https://www.react-spring.dev/docs/components/parallax
 Consider addign some hover animation on the company logo cards wherein if the user hovers over the card, the card either will fade into the details above. Just make sure to make it onClick (rather than onHover) when on mobile. Use the isOnMobile react library for this one. This one is a great example: https://codesandbox.io/p/sandbox/animated-card-component-using-reactjs-yvhil?file=%2Fsrc%2Findex.js
 Alternatively, we can make clicking the card result in expanding it with further information  
 
-- The "About" section's text left-aligns when in Mobile, fix that. 
-
-- We'll really need to clean and consolidate our CSS and styling. 
-
-- Some of the commonly used codes: 
--> hoverEffect object
--> the "header" style
--> useTheme (though we're not sure if we can consolidate it, doesn't hurt to try though)
-
 - Inspect the parts where the code generates repitive components. (E.g., the marquee, the company cards, especially the dialog cards) and see if we can simplify that code by employing the map() function. Just like what we did with the marquee
-
-- Failed to match the forecast and inventory card. Figure it out later. Look at the card in the dialog, they might give you an idea. You may also need to adjust the height of the forecast background image. you may also need to make it a flex container, which would stretch the button to the full width of the card. You may also consider resorting to making the cards the same size
 
 - Make your alt texts more descriptive
 
-- The forecast and the inventory cards are asymmetric. Think about whether you want to make them symmetric (both get 5.5 width or something close to that) or if you want to keep them asymmetric as a styling thing. 
-
 - Add a button that takes you all the way up
-
-- for the phone's slideshow, one of the libraries you could use is react slick. But search for slideshow libraries that are targeting mobile platforms.
-
-- The border color of the form isn't exactly like the standard color COLORS.lightBlue, think about whether you need to change it to the standard color or is it that no one will notice and it's not worth it? 
 
 - The correct way to solve the problem in which the height of the form's card changes for a split second is to set a constant height for the card when the user submits the form. And then set the height back to "auto" when the form re-appears. We've added  the onExited and onEntered functions to the form's CSSTransition group. It sort of fixed the problem of the split second rendering. But it works sporadically. Look into it later, but for now, it's good.
 
@@ -63,6 +42,8 @@ Alternatively, we can make clicking the card result in expanding it with further
 - Make sure we have non-skipping headers. In the About section, the "Hello" and "My name is Naif Abdullah" should realisticly have h2, but they're using h3. Re-evaluate whether this is okay. Though h2 is way too big
 
 - Dig through your hard drive for professional photos to add
+
+- See if you can do away with the gallery's css and make it css in js instead
 */
 
 import { useState } from "react";
