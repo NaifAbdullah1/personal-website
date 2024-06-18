b123bfe4c31ea07c85e1a59cc7ddc45fcbdb56ab
NaifKAbdullah <Naif-Abdullah@outlook.sa>
2024-06-18 14:00:43 +0300

    saving progress. Installed rollup to add the preserveDirectives() configuration to vite.config.js. Also, we're now able to run npx source-map-explorer dist/assets/index-PNAESFdw.js --no-border-checks for analyzing our bundle size

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+// Prevent source map errors from appearing when building the app
+import preserveDirectives from "rollup-preserve-directives";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), preserveDirectives()],
   server: {
     // This attribute allows us to access the site within our home network
     host: "192.168.100.168", // Making the server accessible within my home's IP address
